e1820be9b097c5e0e02a3f8ff43594fd2d4e8fa8
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2017-07-31 18:55:44 -0400

    use google library

--- src/form/services/address/address.service.ts
+++ src/form/services/address/address.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
-import * as GoogleMap from '@google/maps';
+declare var google: any;
 
 @Injectable()
 export class AddressService {
   private googleMapClient;
   constructor() {
-    this.googleMapClient = GoogleMap.createClient({
-      key: 'AIzaSyDUKpmFw-tv25dcoqRhmn8194e9E2tZ6nc'
-    });
+    this.googleMapClient = new google.maps.places.AutocompleteService();
   }
 
   public getAutoComplete(input: string) {
     const resultObservable = new Subject<any[]>();
-    this.googleMapClient.placesAutoComplete({
+    this.googleMapClient.getPlacePredictions({
       input: input,
-    }, (err, response) => {
-      if (!err) {
-        resultObservable.next(response.json.predictions);
-      }
+    }, (response) => {
+      resultObservable.next(response);
     });
     return resultObservable.map((responses) => {
       const result: string[] = [];
